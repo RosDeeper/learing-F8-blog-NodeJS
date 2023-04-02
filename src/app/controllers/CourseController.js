2fc3b28464dff708599a3b7a914bbf1f0f1a1116
RosDeeper <hoanggg237@gmail.com>
2023-04-02 18:40:28 +0700

    create new course, upload course view page

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -2,12 +2,29 @@ const Course = require('../models/course');
 const {singleToObject} = require('../../util/mongoose');
 
 class CourseController {
+    //[GET] courses/:slug
     show(req, res, next)
     {
         Course.findOne({slug: req.params.slug})
             .then((course) => {
                 res.render('courses/show', {course: singleToObject(course)});
             })
+            .catch(next);
+    }
+
+    //[GET] courses/create
+    create(req, res, next)
+    {
+        res.render('courses/create');
+    }
+
+    //[POST] courses/store
+    store(req, res, next)
+    {
+        //res.json(req.body);
+        const course = new Course(req.body);
+        course.save()
+            .then(res.redirect('/'))
             .catch(next);
     }
 }
--- src/app/models/course.js
+++ src/app/models/course.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose');
+const slug = require('mongoose-slug-updater');
+
 const Schema = mongoose.Schema;
+mongoose.plugin(slug);
 
 const Course = new Schema({
-    name: {type: String},
+    name: {type: String, require: true},
     image: {type: String},
-    createAt: {type: Date, default: Date.now},
-    updateAt: {type: Date, default: Date.now},
+    slug: {type: String, slug: 'name', unique: true},
+    videoID: {type: String, require: true},
+}, { 
+    timestamps: true
 });
 
 module.exports = mongoose.model('Course', Course);
