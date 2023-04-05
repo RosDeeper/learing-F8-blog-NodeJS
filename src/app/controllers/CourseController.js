959f395c7d6b0d5c51e22bdf14da171705c289e5
RosDeeper <hoanggg237@gmail.com>
2023-04-06 00:25:07 +0700

    delete courses

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -47,6 +47,16 @@ class CourseController {
             })
             .catch(next);
     }
+
+    //[DELETE]courses/:id
+    remove(req, res, next)
+    {
+        Course.deleteOne({_id: req.params.id})
+            .then(() => {
+                res.redirect('back');
+            })
+            .catch(next);
+    }
 }
 
 module.exports = new CourseController;
--- src/routes/course.js
+++ src/routes/course.js
@@ -4,9 +4,10 @@ const router = express.Router();
 const courseController = require('../app/controllers/CourseController');
 
 router.get('/create', courseController.create);
-router.get('/:id/edit', courseController.edit);
 router.post('/store', courseController.store);
+router.get('/:id/edit', courseController.edit);
 router.put('/:id', courseController.update);
+router.delete('/:id', courseController.remove);
 router.get('/:slug', courseController.show);
 
 module.exports = router;
